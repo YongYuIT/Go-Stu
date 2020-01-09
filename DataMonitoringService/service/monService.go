81de7f104d171ade31c808c7d89d092800b61365
yong <1198749126@qq.com>
2020-01-09 03:54:07 -0800

    try db conn create

--- DataMonitoringService/service/monService.go
+++ DataMonitoringService/service/monService.go
@@ -9,16 +9,11 @@ type MonService struct {
 }
 
 func (thiz *MonService) StartService() {
-	dbItems, err := tools.GetDBConfig()
-	if err != nil {
-		fmt.Println("err-->", err)
-	} else {
-		fmt.Println("dbItems-->", dbItems)
-	}
 	tabIntems, err := tools.GetZeroTabMonConf()
 	if err != nil {
 		fmt.Println("err-->", err)
 	} else {
 		fmt.Println("tabIntems-->", tabIntems)
 	}
+
 }
--- DataMonitoringService/tools/dbTool.go
+++ DataMonitoringService/tools/dbTool.go
@@ -1,9 +1,94 @@
 package tools
 
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"strings"
+)
+
 func ReadTabsUnderSham(sham_name string) []string {
 	return nil
 }
 
-func getDBConn() {
+var dbConnContext map[string]*gorm.DB
 
+func init() {
+	dbItems, err := GetDBConfig()
+	if err != nil {
+		fmt.Println("err-->", err)
+		return
+	} else {
+		fmt.Println("dbItems-->", dbItems)
+	}
+	for i := 0; i < len(dbItems); i++ {
+		host := strings.Split(dbItems[i].IPPort, ":")[0]
+		port := strings.Split(dbItems[i].IPPort, ":")[1]
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, dbItems[i].UserName, dbItems[i].Passwd, dbItems[i].Name)
+		conn, err := gorm.Open("postgres", psqlInfo)
+		if err != nil {
+			dbConnContext[ dbItems[i].Name] = conn
+		}
+	}
 }
+
+func GetConn(name string) *gorm.DB {
+	return dbConnContext[name]
+}
+
+/*
+docker run --name YongPG1 -e POSTGRES_PASSWORD=123456 -e POSTGRES_USER=yuyong -p 5432:5432 -d postgres
+docker run --name YongPG2 -e POSTGRES_PASSWORD=123456 -e POSTGRES_USER=yuyong -p 5433:5432 -d postgres
+docker ps -a | grep post
+
+create database db1;
+create schema sch1;
+grant all on schema sch1 to yuyong;
+create table sch1.test1(
+    f_stu_name varchar(255),
+    f_stu_age varchar(255)
+);
+grant all on sch1.test1 to yuyong;
+create table sch1.test2(
+    f_stu_name varchar(255),
+    f_stu_age varchar(255)
+);
+grant all on sch1.test2 to yuyong;
+create table sch1.test3(
+    f_stu_name varchar(255),
+    f_stu_age varchar(255)
+);
+grant all on sch1.test3 to yuyong;
+
+insert into sch1.test1(f_stu_name,f_stu_age)
+values('aaa','111'),
+      ('bbb','222'),
+      ('ccc','333');
+
+insert into sch1.test2(f_stu_name,f_stu_age)
+values('aaa','111'),
+      ('bbb','222'),
+      ('bbb','444'),
+      ('ccc','333');
+
+insert into sch1.test3(f_stu_name,f_stu_age)
+values('aaa','111'),
+      ('bbb','222'),
+      ('ccc','333');
+
+----------------------------------------------------------------
+
+create database db2;
+create schema sch2;
+grant all on schema sch2 to yuyong;
+create table sch2.test1(
+    f_stu_name varchar(255),
+    f_stu_age varchar(255)
+);
+grant all on sch2.test1 to yuyong;
+create table sch2.test2(
+    f_stu_name varchar(255),
+    f_stu_age varchar(255)
+);
+grant all on sch2.test2 to yuyong;
+*/
