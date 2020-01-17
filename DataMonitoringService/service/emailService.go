b18257a534cef348aab415d28a625835e7fe8323
yong <1198749126@qq.com>
2020-01-16 21:51:13 -0800

    try Go html/template

--- DataMonitoringService/service/emailService.go
+++ DataMonitoringService/service/emailService.go
@@ -1,11 +1,20 @@
 package service
 
+import (
+	"../tools"
+	"strings"
+)
+
 type EmailService struct {
 }
 
 func (thiz *EmailService) sendSimpleSchaInfoEmail(scha_info string) {
 
+	fileTool := &tools.FileTool{}
 	pService := PrintService{}
 	linFileName := pService.PrintSchaPolylines(scha_info)
-
+	viewHtml := fileTool.ReadFileTxt("../view/SimpSchaEmailView.html")
+	viewHtml = strings.Replace(viewHtml, "##$text_content$##", "fuck worning", -1)
+	lineFileStr := fileTool.ReadFileTxt(linFileName)
+	viewHtml = strings.Replace(viewHtml, "##$html_content$##", lineFileStr, -1)
 }
