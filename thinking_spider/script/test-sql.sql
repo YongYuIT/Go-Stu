f2eae9f84664c629c3049ea54feae131234c70e2
yong <1198749126@qq.com>
2021-04-22 00:57:01 -0700

    add new release

--- thinking_spider/script/test-sql.sql
+++ thinking_spider/script/test-sql.sql
@@ -1,4 +1,4 @@
 select t.key_word, sum(t.`range`) all_sum,avg(t.price),count(*),sum(t.`range`)/count(*)
-from key_word_prod_records t
+from `key_word_prod_records-bak20210407001` t
 group by t.key_word
 order by all_sum desc
