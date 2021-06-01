f2b2a2ef822000b2c70b568043b262e4233415c4
yong <1198749126@qq.com>
2021-06-01 02:04:14 -0700

    add insert for detail

--- thinking_spider/model/prod_detail_record.go
+++ thinking_spider/model/prod_detail_record.go
@@ -25,6 +25,9 @@ func SaveProdDetailRecord(record *ProdDetailRecord) {
 	if !database.CurrentDB.HasTable(record) {
 		database.CurrentDB.AutoMigrate(record)
 	}
+	if record == nil {
+		return
+	}
 	if database.CurrentDB.NewRecord(record) {
 		fmt.Println("create record-->", record.ID)
 		database.CurrentDB.Create(record)
@@ -40,6 +43,7 @@ type AsinUrl struct {
 }
 
 func GetUrlByKeyWords(keyword string) *[]AsinUrl {
+	SaveProdDetailRecord(nil)
 	getUrlByAsin := fmt.Sprintf("select detial_url, asin\nfrom (select detial_url, asin, row_number() over (partition by asin order by created_at desc) as aindex\n      from key_word_prod_records_source\n      where key_word = '%s'\n        and asin not in (select asin from prod_detail_records)) t\nwhere t.aindex = 1\n  and detial_url != ''", keyword)
 	var asinUrls []AsinUrl
 	database.CurrentDB.Raw(getUrlByAsin).Scan(&asinUrls)
